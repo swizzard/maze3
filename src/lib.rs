af2ad1e8d9b1717f73204364f4f7d4e23c46339a
Sam Raker <sam.raker@gmail.com>
2025-05-15 18:33:38 -0400

    random walk

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub enum DoorState {
     Closed,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Doors {
     pub north: Option<DoorState>,
     pub east: Option<DoorState>,
@@ -122,7 +122,7 @@ pub struct Room {
 
 impl std::fmt::Display for Room {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "{}\n", self.doors)?;
+        writeln!(f, "{}", self.doors)?;
         write!(f, "{}", self.description)
     }
 }
@@ -131,6 +131,7 @@ impl std::fmt::Display for Room {
 pub struct Maze<const N_ROWS: usize, const N_COLS: usize> {
     pub rooms: V2<Room, N_ROWS, N_COLS>,
     pub current_ix: BoundedIx2<N_ROWS, N_COLS>,
+    pub goal: BoundedIx2<N_ROWS, N_COLS>,
 }
 
 impl<const N_ROWS: usize, const N_COLS: usize> Maze<N_ROWS, N_COLS> {
@@ -142,68 +143,18 @@ impl<const N_ROWS: usize, const N_COLS: usize> Maze<N_ROWS, N_COLS> {
                 description: format!("room {ix:?}"),
                 doors: Doors {
                     north: ix.north().map(|_| DoorState::Closed),
-                    east: ix.west().map(|_| DoorState::Closed),
+                    east: ix.east().map(|_| DoorState::Closed),
                     south: ix.south().map(|_| DoorState::Closed),
-                    west: ix.east().map(|_| DoorState::Closed),
+                    west: ix.west().map(|_| DoorState::Closed),
                 },
             };
             rooms.push(r);
         }
-        let rooms: V2<Room, N_ROWS, N_COLS> = V2::new(rooms).unwrap();
         Self {
-            rooms,
+            rooms: V2::new(rooms).unwrap(),
             current_ix: BoundedIx2::new(0, 0).unwrap(),
+            goal: BoundedIx2::max(),
         }
-    }
-    pub fn fprint<W: Write>(&self, st: &mut W) -> Result<(), std::fmt::Error> {
-        for row in iterators::BoundedIx2Rows::<N_ROWS, N_COLS>::new() {
-            for ix in row {
-                let n = match self.rooms[ix].doors.north {
-                    None => "\u{2500}",
-                    Some(DoorState::Closed) => "\u{2505}",
-                    Some(DoorState::Open) => "\u{2571}",
-                };
-                write!(st, "\u{250c}\u{2500}{n}\u{2500}\u{2510}")?;
-            }
-            st.write_char('\n')?;
-            for _ in row {
-                write!(st, "\u{2502}   \u{2502}")?;
-            }
-            st.write_char('\n')?;
-            for ix in row {
-                let e = match self.rooms[ix].doors.east {
-                    None => "\u{2502}",
-                    Some(DoorState::Closed) => "\u{250b}",
-                    Some(DoorState::Open) => "\u{2571}",
-                };
-                let w = match self.rooms[ix].doors.west {
-                    None => "\u{2502}",
-                    Some(DoorState::Closed) => "\u{250b}",
-                    Some(DoorState::Open) => "\u{2572}",
-                };
-                let c = if ix == self.current_ix {
-                    '\u{00b7}'
-                } else {
-                    ' '
-                };
-                write!(st, "{e} {c} {w}")?;
-            }
-            st.write_char('\n')?;
-            for _ in row {
-                write!(st, "\u{2502}   \u{2502}")?;
-            }
-            st.write_char('\n')?;
-            for ix in row {
-                let s = match self.rooms[ix].doors.south {
-                    None => "\u{2500}",
-                    Some(DoorState::Closed) => "\u{2505}",
-                    Some(DoorState::Open) => "\u{2571}",
-                };
-                write!(st, "\u{2514}\u{2500}{s}\u{2500}\u{2518}")?;
-            }
-            st.write_char('\n')?;
-        }
-        Ok(())
     }
     pub fn seed_doors_naive(&mut self) {
         let mut rng = rand::rng();
@@ -219,6 +170,28 @@ impl<const N_ROWS: usize, const N_COLS: usize> Maze<N_ROWS, N_COLS> {
             }
             if rng.random_bool(0.5) {
                 self.open_west(ix);
+            }
+        }
+        // ensure we can get out of starting room
+        let start_ix = BoundedIx2::min();
+        if self.rooms[start_ix].doors.east == Some(DoorState::Closed)
+            && self.rooms[start_ix].doors.south == Some(DoorState::Closed)
+        {
+            if rng.random_bool(0.5) {
+                self.open_west(start_ix);
+            } else {
+                self.open_south(start_ix);
+            }
+        }
+        // ensure we can get into end room
+        let end_ix = BoundedIx2::max();
+        if self.rooms[end_ix].doors.west == Some(DoorState::Closed)
+            && self.rooms[end_ix].doors.north == Some(DoorState::Closed)
+        {
+            if rng.random_bool(0.5) {
+                self.open_east(end_ix);
+            } else {
+                self.open_north(end_ix);
             }
         }
     }
@@ -270,10 +243,235 @@ impl<const N_ROWS: usize, const N_COLS: usize> Maze<N_ROWS, N_COLS> {
             r.doors.close_east();
         }
     }
+    pub fn move_north(&mut self) -> bool {
+        match self.rooms[self.current_ix].doors.north {
+            Some(DoorState::Open) => {
+                self.current_ix = self.current_ix.north().unwrap();
+                true
+            }
+            _ => false,
+        }
+    }
+    pub fn move_south(&mut self) -> bool {
+        match self.rooms[self.current_ix].doors.south {
+            Some(DoorState::Open) => {
+                self.current_ix = self.current_ix.south().unwrap();
+                true
+            }
+            _ => false,
+        }
+    }
+    pub fn move_east(&mut self) -> bool {
+        match self.rooms[self.current_ix].doors.east {
+            Some(DoorState::Open) => {
+                self.current_ix = self.current_ix.east().unwrap();
+                true
+            }
+            _ => false,
+        }
+    }
+    pub fn move_west(&mut self) -> bool {
+        match self.rooms[self.current_ix].doors.west {
+            Some(DoorState::Open) => {
+                self.current_ix = self.current_ix.west().unwrap();
+                true
+            }
+            _ => false,
+        }
+    }
+    pub fn fprint<W: Write>(&self, st: &mut W) -> Result<(), std::fmt::Error> {
+        for row in iterators::BoundedIx2Rows::<N_ROWS, N_COLS>::new() {
+            for ix in row {
+                let n = match self.rooms[ix].doors.north {
+                    None => "\u{2500}",
+                    Some(DoorState::Closed) => "\u{2505}",
+                    Some(DoorState::Open) => "\u{2571}",
+                };
+                write!(st, "\u{250c}\u{2500}{n}\u{2500}\u{2510}")?;
+            }
+            st.write_char('\n')?;
+            for _ in row {
+                write!(st, "\u{2502}   \u{2502}")?;
+            }
+            st.write_char('\n')?;
+            for ix in row {
+                let e = match self.rooms[ix].doors.east {
+                    None => "\u{2502}",
+                    Some(DoorState::Closed) => "\u{250b}",
+                    Some(DoorState::Open) => "\u{2571}",
+                };
+                let w = match self.rooms[ix].doors.west {
+                    None => "\u{2502}",
+                    Some(DoorState::Closed) => "\u{250b}",
+                    Some(DoorState::Open) => "\u{2571}",
+                };
+                let c = if ix == self.current_ix {
+                    '\u{2605}'
+                } else if ix == self.goal {
+                    '\u{2698}'
+                } else {
+                    ' '
+                };
+                write!(st, "{w} {c} {e}")?;
+            }
+            st.write_char('\n')?;
+            for _ in row {
+                write!(st, "\u{2502}   \u{2502}")?;
+            }
+            st.write_char('\n')?;
+            for ix in row {
+                let s = match self.rooms[ix].doors.south {
+                    None => "\u{2500}",
+                    Some(DoorState::Closed) => "\u{2505}",
+                    Some(DoorState::Open) => "\u{2571}",
+                };
+                write!(st, "\u{2514}\u{2500}{s}\u{2500}\u{2518}")?;
+            }
+            st.write_char('\n')?;
+        }
+        Ok(())
+    }
+    pub fn pprint(&self) -> String {
+        // each room is 5 characters wide and 5 characters high, plus newlines
+        let cap = 25 * N_COLS * N_ROWS + N_COLS;
+        let mut s = String::with_capacity(cap);
+        self.fprint(&mut s).unwrap();
+        s
+    }
 }
 
 impl<const N_ROWS: usize, const N_COLS: usize> Default for Maze<N_ROWS, N_COLS> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use multid::BoundedIx2;
+
+    #[test]
+    fn test_new() {
+        let m = Maze::<3, 3>::new();
+        assert_eq!(
+            Doors {
+                north: None,
+                east: Some(DoorState::Closed),
+                south: Some(DoorState::Closed),
+                west: None,
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(0, 0).unwrap()].doors,
+            "0, 0"
+        );
+        assert_eq!(
+            Doors {
+                north: None,
+                east: Some(DoorState::Closed),
+                south: Some(DoorState::Closed),
+                west: Some(DoorState::Closed),
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(0, 1).unwrap()].doors,
+            "0, 1"
+        );
+        assert_eq!(
+            Doors {
+                north: None,
+                east: None,
+                south: Some(DoorState::Closed),
+                west: Some(DoorState::Closed),
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(0, 2).unwrap()].doors,
+            "0, 2"
+        );
+        assert_eq!(
+            Doors {
+                north: Some(DoorState::Closed),
+                east: Some(DoorState::Closed),
+                south: Some(DoorState::Closed),
+                west: None,
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(1, 0).unwrap()].doors,
+            "1, 0"
+        );
+        assert_eq!(
+            Doors {
+                north: Some(DoorState::Closed),
+                east: Some(DoorState::Closed),
+                south: Some(DoorState::Closed),
+                west: Some(DoorState::Closed),
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(1, 1).unwrap()].doors,
+            "1, 1"
+        );
+        assert_eq!(
+            Doors {
+                north: Some(DoorState::Closed),
+                east: None,
+                south: Some(DoorState::Closed),
+                west: Some(DoorState::Closed),
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(1, 2).unwrap()].doors,
+            "1, 2"
+        );
+        assert_eq!(
+            Doors {
+                north: Some(DoorState::Closed),
+                east: Some(DoorState::Closed),
+                south: None,
+                west: None,
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(2, 0).unwrap()].doors,
+            "2,0"
+        );
+        assert_eq!(
+            Doors {
+                north: Some(DoorState::Closed),
+                east: Some(DoorState::Closed),
+                south: None,
+                west: Some(DoorState::Closed),
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(2, 1).unwrap()].doors,
+            "2,1"
+        );
+        assert_eq!(
+            Doors {
+                north: Some(DoorState::Closed),
+                east: None,
+                south: None,
+                west: Some(DoorState::Closed),
+            },
+            m.rooms[BoundedIx2::<3, 3>::new(2, 2).unwrap()].doors,
+            "2,2"
+        );
+    }
+
+    #[test]
+    fn test_open_east() {
+        let mut m = Maze::<3, 3>::new();
+        let ix = BoundedIx2::<3, 3>::new(0, 0).unwrap();
+        dbg!(&m.rooms[ix].doors);
+        m.open_east(ix);
+        assert_eq!(
+            Some(DoorState::Open),
+            m.rooms[ix].doors.east,
+            "original room"
+        );
+        let ix2 = BoundedIx2::<3, 3>::new(0, 1).unwrap();
+        assert_eq!(Some(DoorState::Open), m.rooms[ix2].doors.west, "neighbor");
+    }
+    #[test]
+    fn test_open_west() {
+        let mut m = Maze::<3, 3>::new();
+        let ix = BoundedIx2::<3, 3>::new(0, 1).unwrap();
+        dbg!(&m.rooms[ix].doors);
+        m.open_west(ix);
+        assert_eq!(
+            Some(DoorState::Open),
+            m.rooms[ix].doors.west,
+            "original room"
+        );
+        let ix2 = BoundedIx2::<3, 3>::new(0, 0).unwrap();
+        assert_eq!(Some(DoorState::Open), m.rooms[ix2].doors.east, "neighbor");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use maze3::*;
-use std::io::{Write, stdout};
+use rand::distr::StandardUniform;
+use rand::prelude::*;
+
+fn random_step<const N_ROWS: usize, const N_COLS: usize>(
+    maze: &mut Maze<N_ROWS, N_COLS>,
+    rng: &mut ThreadRng,
+) {
+    let mut moved = false;
+    while !moved {
+        let v: u8 = rng.sample::<u8, StandardUniform>(StandardUniform) % 4;
+        moved = match v {
+            0 => maze.move_north(),
+            1 => maze.move_south(),
+            2 => maze.move_east(),
+            3 => maze.move_west(),
+            _ => panic!("unreachable"),
+        }
+    }
+}
+
 fn main() {
-    let mut m = Maze::<3, 3>::new();
+    let mut m = Maze::<4, 4>::new();
+    // m.open_east(BoundedIx2::<3, 3>::new(0, 0).unwrap());
     m.seed_doors_naive();
     // println!("{:?}", m.rooms[m.current_ix]);
-    let mut s = String::new();
-    m.fprint(&mut s).unwrap();
-    let mut st = stdout();
-    write!(st, "{s}").unwrap();
+    // let m = Maze::<3, 3>::new();
+    println!("start\n{}", m.pprint());
+    let mut rng = rand::rng();
+    let mut steps = 0;
+    while m.current_ix != m.goal && steps < 51 {
+        random_step(&mut m, &mut rng);
+        steps += 1;
+        println!("{}", m.pprint())
+    }
+    println!("done in {steps} steps");
 }
